793282525a9139045ec5219f0fedf74a0e82c69e
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-12-13 21:03:31 +0100

    update shell-based installation instructions

--- starknetup.sh
+++ starknetup.sh
@@ -131,22 +131,40 @@ ensure() {
 install_asdf_interactively() {
     local _profile
     local _pref_shell
+    local _asdf_dir="$HOME/.asdf"
+
     case ${SHELL:-""} in
         */zsh)
             _profile=$HOME/.zshrc
             _pref_shell=zsh
+            ;;
+        */fish)
+            _profile=$HOME/.config/fish/config.fish
+            _pref_shell=fish
+            ;;
+        */elvish)
+            _profile=$HOME/.config/elvish/rc.elv
+            _pref_shell=elvish
+            ;;
+        */pwsh)
+            _profile=$HOME/.config/powershell/profile.ps1
+            _pref_shell=pwsh
+            ;;
+        */nu)
+            _profile=$HOME/.config/nushell/config.nu
+            _pref_shell=nu
             ;;
         */ash)
             _profile=$HOME/.profile
             _pref_shell=ash
             ;;
         */bash)
-            _profile=$HOME/.bashrc
+            if [ "$(uname)" = "Darwin" ]; then
+                _profile=$HOME/.bash_profile
+            else
+                _profile=$HOME/.bashrc
+            fi
             _pref_shell=bash
-            ;;
-        */fish)
-            _profile=$HOME/.config/fish/config.fish
-            _pref_shell=fish
             ;;
         *)
             err "could not detect shell, manually add '${LOCAL_BIN_ESCAPED}' to your PATH."
@@ -154,16 +172,42 @@ install_asdf_interactively() {
     esac
 
     if [ -n "$_profile" ]; then
+        if [ ! -f "$_profile" ]; then
+            touch "$_profile"
+        fi
+
         printf "asdf-vm is required. Do you want to install it now? (y/N): "
         read -r answer
         case $answer in
             [Yy]* )
                 printf "Installing asdf-vm...\n"
-                git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
-                echo >>"$_profile" && echo ". \$HOME/.asdf/asdf.sh" >>"$_profile"
-                echo >>"$_profile" && echo ". \$HOME/.asdf/completions/asdf.bash" >>"$_profile"
+                git clone https://github.com/asdf-vm/asdf.git "$_asdf_dir" --branch v0.14.1
+
+                case $_pref_shell in
+                    zsh|bash|ash)
+                        echo >>"$_profile" && echo ". ${_asdf_dir}/asdf.sh" >>"$_profile"
+                        echo >>"$_profile" && echo ". ${_asdf_dir}/completions/asdf.bash" >>"$_profile"
+                        ;;
+                    fish)
+                        echo >>"$_profile" && echo "source ${_asdf_dir}/asdf.fish" >>"$_profile"
+                        mkdir -p "$HOME/.config/fish/completions"
+                        ln -s "${_asdf_dir}/completions/asdf.fish" "$HOME/.config/fish/completions"
+                        ;;
+                    elvish)
+                        echo >>"$_profile" && echo "use asdf _asdf; var asdf~ = $_asdf:asdf~" >>"$_profile"
+                        echo >>"$_profile" && echo "set edit:completion:arg-completer[asdf] = $_asdf:arg-completer~" >>"$_profile"
+                        ;;
+                    pwsh)
+                        echo >>"$_profile" && echo ". '${_asdf_dir}/asdf.ps1'" >>"$_profile"
+                        ;;
+                    nu)
+                        echo >>"$_profile" && echo "\$env.ASDF_DIR = '${_asdf_dir}'" >>"$_profile"
+                        echo >>"$_profile" && echo "source '${_asdf_dir}/asdf.nu'" >>"$_profile"
+                        ;;
+                esac
+
                 printf "asdf-vm has been installed. Please restart your shell for the changes to take effect.\n"
-				exit 0
+                exit 0
                 ;;
             * )
                 err "asdf-vm is required. Please install it manually and re-run this script. Refer to ${ASDF_REPO} for installation instructions."
